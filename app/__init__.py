9236dc89d562791c987da625c900afba6b73039a
Ojas Saxena <ojassaxena03@gmail.com>
2020-04-23 02:55:22 +0530

    added mysqldb

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_PASSWORD"] = 'ojasva392000'
+app.config["MYSQL_DB"] = 'DBMS'
+app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
+mysql = MySQL(app)
 
 from app import routes
