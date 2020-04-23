b552566834c0a0e44f42736a579439378497f5aa
Ojas Saxena <ojassaxena03@gmail.com>
2020-04-23 23:06:48 +0530

    update

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 from flask import render_template , flash , redirect , url_for , request
 from app import app , mysql
 
+# cur = mysql.connection.cursor()
+# cur.execute("select First_Name , Grade from Students where Grade < 5.25")
+# rv = str(cur.fetchall())
+
 @app.route('/')
 def homepage() :
-    cur = mysql.connection.cursor()
-    cur.execute("select * from Branch")
-    rv = str(cur.fetchall())
-    return render_template("index.html" , value = rv)
+    return render_template("homepage.html" , value = "")
+
+@app.route('/students')
+def showStudents() :
+    return render_template("students.html" , value = "")
