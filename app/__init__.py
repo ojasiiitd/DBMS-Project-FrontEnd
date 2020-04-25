2fdc20101397dbd186996af5f45dbbfec82f82fa
Ojas Saxena <ojassaxena03@gmail.com>
2020-04-26 00:07:51 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = 'localhost'
 app.config["MYSQL_USER"] = 'root'
-app.config["MYSQL_PASSWORD"] = 'ojasva392000'
+app.config["MYSQL_PASSWORD"] = '1234'
 app.config["MYSQL_DB"] = 'DBMS'
 app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
 mysql = MySQL(app)
